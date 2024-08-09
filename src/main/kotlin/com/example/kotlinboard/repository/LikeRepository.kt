9e90a16039aced6e7706bb3006d4fe61b6500786
Lee Jae Hun <wogns970@naver.com>
2024-08-09 16:45:09 +0900

    좋아요 카운팅
    * ktlint 적용
    * 테스트 정상 작동 확인

--- src/main/kotlin/com/example/kotlinboard/repository/LikeRepository.kt
+++ src/main/kotlin/com/example/kotlinboard/repository/LikeRepository.kt
@@ -3,4 +3,6 @@ package com.example.kotlinboard.repository
 import com.example.kotlinboard.domain.Like
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface LikeRepository : JpaRepository<Like, Long>
+interface LikeRepository : JpaRepository<Like, Long> {
+    fun countByPostId(postId: Long): Long
+}
